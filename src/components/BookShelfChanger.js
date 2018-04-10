d36142adcbe8cb58a9582a706ecc7fcc15a5a944
Rory Sterley <rsterley@o3international.com>
2018-04-10 10:35:47 -0700

    Search function uses state rather then getAll whenever possible

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,31 +1,6 @@
 import React from 'react'
 import * as BooksAPI from './../BooksAPI'
 
-// const BookShelfChanger = (props) => {
-//   let handleChange = e => {
-//     let book = { id: props.id }
-//     let shelf = e.target.value
-// 
-//     BooksAPI.update(book, shelf)
-//       .then((res) => {
-//         // re-render page
-//         props.callback()
-//       })
-//   }
-// 
-//   return (
-//     <div className="book-shelf-changer">
-//       <select value={props.shelf} onChange={handleChange}>
-//         <option value="none" disabled>Move to...</option>
-//         <option value="currentlyReading">Currently Reading</option>
-//         <option value="wantToRead">Want to Read</option>
-//         <option value="read">Read</option>
-//         <option value="none">None</option>
-//       </select>
-//     </div>
-//   )
-// }
-
 class BookShelfChanger extends React.Component {
   theShelf = this.props.shelf
 
